c1f100a3756abef5616ae1abcdff1223452142bd
whiteAnt001 <bakjs01@gmail.com>
2026-02-11 22:45:29 +0900

    chore: JWT Cookie 관리용 유틸 클래스 구현(#13)

---
+++ demo/src/main/java/util/CookieUtil.java
@@ -0,0 +1,28 @@
+package util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+    public static final String ACCESS_TOKEN_COOKIE = "access_token";
+
+    public void addAccessTokenCookie(HttpServletResponse response, String accessToken) {
+        Cookie cookie = new Cookie(ACCESS_TOKEN_COOKIE, accessToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false); //https일경우엔 true로 해야함
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 15);
+
+        response.addCookie(cookie);
+    }
+
+    public void deleteAccessTokenCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie(ACCESS_TOKEN_COOKIE, null);
+        cookie.setPath("/");
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+    }
+
+}
